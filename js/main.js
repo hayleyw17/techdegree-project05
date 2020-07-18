7307c6f03df49cf3a2c4968afbdbb9f248d1a956
hayleyw17 <64605209+hayleyw17@users.noreply.github.com>
2020-07-17 21:48:01 -0400

    Added search bar JS

--- js/main.js
+++ js/main.js
@@ -8,4 +8,20 @@ lightbox.option({
 
 // Search Features
 
-
+const captionArray = [];
+const search = document.getElementById('search');
+const galleryItem = document.querySelectorAll('.gallery a');
+for (let i = 0; i < galleryItem.length; i ++) {
+    let data = galleryItem[i].getAttribute('data-title').toLowerCase();
+    captionArray.push(data);
+}
+search.addEventListener('keyup', () => {
+    let value = search.value.toLowerCase();
+    for (let i = 0; i < captionArray.length; i++) {
+        if (captionArray[i].includes(value)) {
+            galleryItem[i].style.display = 'block';
+        } else {
+            galleryItem[i].style.display = 'none';
+        }
+    }
+});
